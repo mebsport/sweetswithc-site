b5fdc1f9c567c524317c15f777df67503d1e2021
mebsport <56307645+mebsport@users.noreply.github.com>
2024-09-08 17:35:45 -0400

    created order and product pages

---
+++ src/app/order/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import NavBar from '../components/NavBar'
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col bg-black'>
+      <NavBar />
+      <div className='container mt-24 mx-auto py-4 px-12'>
+        <NavBar />
+      </div>
+    </main>
+  )
+}
