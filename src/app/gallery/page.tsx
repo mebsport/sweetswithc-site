1629a3ec3054018283b165db05ddfce88ffd42b5
mebsport <56307645+mebsport@users.noreply.github.com>
2024-09-08 14:07:43 -0400

    Create Gallery Page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import NavBar from '../components/NavBar'
+import ImageGallery from '../components/ImageGallery'
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col bg-black'>
+      <NavBar />
+      <div className='container mt-24 mx-auto py-4 px-12'>
+        <NavBar />
+        <ImageGallery/>
+      </div>
+    </main>
+  )
+}
