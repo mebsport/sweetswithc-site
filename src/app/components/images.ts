2212621f022a33714559ad662729cf2f3e64be00
mebsport <56307645+mebsport@users.noreply.github.com>
2024-09-08 14:07:36 -0400

    Create Image List

---
+++ src/app/components/images.ts
@@ -0,0 +1,98 @@
+import { Image } from 'react-grid-gallery'
+
+export interface CustomImage extends Image {
+  original: string
+}
+
+export const images: CustomImage[] = [
+  {
+    src: '/Images/GalleryImages/IMG_3089.JPG',
+    original: '/Images/GalleryImages/IMG_3089.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_3104.JPG',
+    original: '/Images/GalleryImages/IMG_3104.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_3127.JPG',
+    original: '/Images/GalleryImages/IMG_3127.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_3152.JPG',
+    original: '/Images/GalleryImages/IMG_3152.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_3168.JPG',
+    original: '/Images/GalleryImages/IMG_3168.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_3784.JPG',
+    original: '/Images/GalleryImages/IMG_3784.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_3789.JPG',
+    original: '/Images/GalleryImages/IMG_3789.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_3796.JPG',
+    original: '/Images/GalleryImages/IMG_3796.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_3816.JPG',
+    original: '/Images/GalleryImages/IMG_3816.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_3817.JPG',
+    original: '/Images/GalleryImages/IMG_3817.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_3865.JPG',
+    original: '/Images/GalleryImages/IMG_3865.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_6427.JPG',
+    original: '/Images/GalleryImages/IMG_6427.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_6604.JPG',
+    original: '/Images/GalleryImages/IMG_6604.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_6619.JPG',
+    original: '/Images/GalleryImages/IMG_6619.JPG',
+    width: 600,
+    height: 800,
+  },
+  {
+    src: '/Images/GalleryImages/IMG_6669.JPG',
+    original: '/Images/GalleryImages/IMG_6669.JPG',
+    width: 600,
+    height: 800,
+  },
+]
